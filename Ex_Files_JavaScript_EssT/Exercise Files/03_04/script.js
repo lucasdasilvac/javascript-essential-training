90b041d58b39fb37ad7d2a5029af69cbceed4a09
lucasdasilvac <lucascosta@alu.ufc.br>
2019-09-02 21:06:15 -0300

    chapter 3 done

--- Ex_Files_JavaScript_EssT/Exercise Files/03_04/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/03_04/script.js
@@ -1,3 +1,8 @@
 var a = 5;
 var b = 4;
 var sum = a + b;
+
+var humanSum = "The value of a: " + a + ". The value of b: " + b + ". The sum of a and b: " +
+sum + ".";
+
+console.log(humanSum);
---
+++ Ex_Files_JavaScript_EssT/Exercise Files/03_07/script.js
@@ -0,0 +1,5 @@
+var pens;
+pens = ["red", "blue", "green", "orange"];
+
+var mixedBag = ["keys", 5, false, 568, "one more thing"];
+console.log(mixedBag);
