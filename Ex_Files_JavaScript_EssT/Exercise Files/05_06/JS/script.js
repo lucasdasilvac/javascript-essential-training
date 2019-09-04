a2d680a038da6d17ca2ceaedd894d84c01491dfe
lucasdasilvac <lucascosta@alu.ufc.br>
2019-09-03 21:03:52 -0300

    chapter 5 done

--- Ex_Files_JavaScript_EssT/Exercise Files/05_06/JS/script.js
+++ Ex_Files_JavaScript_EssT/Exercise Files/05_06/JS/script.js
@@ -1,1 +1,15 @@
 
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
+
+var altText = THEIMAGE.getAttribute("alt");
+
+var captionElement = document.createElement("figcaption");
+
+var captionText = document.createTextNode(altText);
+
+captionElement.appendChild(captionText);
+
+FEATURED.appendChild(captionElement);
+
+THEIMAGE.setAttribute("alt", "");
